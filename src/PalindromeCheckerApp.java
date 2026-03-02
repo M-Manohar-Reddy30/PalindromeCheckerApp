f86087292dd8df4d00f854a02dd69de7157fd699
M-Manohar-Reddy30 <manoharreddyind@gmail.com>
2026-03-02 16:44:55 +0530

    UC2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,20 +1,19 @@
-import java.util.Scanner;
-
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        String input = "madam";
+        boolean isPalindrome = true;
 
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version: 1.0");
-        System.out.println("System initialized successfully.\n");
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
 
-        System.out.print("Please enter a word to check: ");
-        String input = scanner.nextLine();
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
 
-        System.out.println("You entered: " + input);
-
-        scanner.close();
     }
 }
